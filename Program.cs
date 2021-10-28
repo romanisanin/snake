36507c19c5068c80ec6ff382496f5f4e0830e973
romanisanin <romanisanin@gmail.com>
2021-10-27 22:08:20 -0400

    HorizontalLine + VerticalLine

--- Program.cs
+++ Program.cs
@@ -13,41 +13,11 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorisontalLine hLine = new HorisontalLine(5,10,8,'+');
+            hLine.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (var i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            List<Char> charList = new List<Char>();
-            charList.Add('*');
-            charList.Add('.');
-
-            List<int> intList = new List<int>();
-            intList.Add(1);
-            intList.Add(2);
-            intList.Add(3);
-            intList.Add(4);
-
-            foreach (var i in intList)
-            {
-                Console.WriteLine(i + 5);
-            }
+            VerticalLine vLine = new VerticalLine(5, 10, 10, '-');
+            vLine.Draw();
 
             Console.ReadLine();
          }
