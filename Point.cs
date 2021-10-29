90d3773245dd459ca12019272bbba30e59bdb096
romanisanin <romanisanin@gmail.com>
2021-10-28 21:39:51 -0400

    Lesson9/SnakeClass

--- Point.cs
+++ Point.cs
@@ -14,6 +14,32 @@ namespace Snake
         {
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if(direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }else if(direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else
+            {
+                y = y - offset;
+            }
+        }
+
         public Point (int _x, int _y, char _sym)
         {
             x = _x;
@@ -25,6 +51,11 @@ namespace Snake
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
