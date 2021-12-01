bef094a2bf125ae7f07a8180ca0ccc6767112d8b
romanisanin <romanisanin@gmail.com>
2021-11-30 20:37:29 -0500

    Lesson11/ManagableSnake

--- Point.cs
+++ Point.cs
@@ -32,11 +32,11 @@ namespace Snake
                 x = x - offset;
             }else if(direction == Direction.UP)
             {
-                y = y + offset;
+                y = y - offset;
             }
             else
             {
-                y = y - offset;
+                y = y + offset;
             }
         }
 
--- Snake.cs
+++ Snake.cs
@@ -37,5 +37,17 @@ namespace Snake
             newPoint.Move(1, direction);
             return newPoint;
         }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
     }
 }
